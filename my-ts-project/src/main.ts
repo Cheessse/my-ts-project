2b0869b698c5a2ef9f4538962190c5eb418e19a1
Cheessse <davidprekaska12345@gmail.com>
2024-05-11 20:37:04 +0300

    upd

--- my-ts-project/src/main.ts
+++ my-ts-project/src/main.ts
@@ -1,7 +1,13 @@
 import "./style.css";
 
-function add(num1: number, num2: number) {
-  return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
 
-console.log(add(1, 2));
+// console.log(add(1, 2));
+
+type Person = { name: string; age?: number };
+
+let user: Person = { name: "Alice" };
+
+console.log(user);
